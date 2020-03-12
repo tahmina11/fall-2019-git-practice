de3fc9535a0ea30c144861cccb4e9d0d8e1bc235
Tahmina <Tahmina@Tahminas-MacBook-Air.local>
2020-03-11 20:20:12 -0400

    first commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,5 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,5 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("new life new class");
+    }
+}
