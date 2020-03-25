f693b2c27e564b8f7b5a713f32026a37bb22842d
tahmina11 <aripova_tahmina@mail.ru>
2020-03-25 19:14:51 -0400

    tahmina

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,6 +3,7 @@ public class FirstClass {
         System.out.println("hello world");
         System.out.println("Zhuma bulojka");
         System.out.println("Arman kakshaka");
+        System.out.println("\"rock and roll\" = " + "rock and roll");
 
     }
 }
