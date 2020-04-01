1527e3c5fd40bfeacf26c825ba45a44fd64a3c42
tahmina11 <aripova_tahmina@mail.ru>
2020-04-01 19:23:24 -0400

    change

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,5 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("new life new class");
+        System.out.println("Life is good, and it is third online class for github");
     }
 }
